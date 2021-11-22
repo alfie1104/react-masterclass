4aad3c755df8f2ea3a510f756f1b67bf3293b2db
peter_alfie <alfie1104@gmail.com>
2021-11-23 08:58:41 +0900

    #4.13 Price chart is added

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <ReactQueryDevtools initialIsOpen={true} />
+      <ReactQueryDevtools initialIsOpen={false} />
       <Router />
     </>
   );
