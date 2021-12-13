d8a87fb81733df67f7075e017b88e93dcd241ab3
peter_alfie <alfie1104@gmail.com>
2021-12-13 09:05:16 +0900

    #6.4 Styles and Placeholders

--- src/index.tsx
+++ src/index.tsx
@@ -63,7 +63,7 @@ body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
   background-color:${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor};
+  color:black;
   line-height: 1.2;
 }
 a {
@@ -76,9 +76,8 @@ ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
       <ThemeProvider theme={darkTheme}>
-        <GlobalStyle>
-          <App />
-        </GlobalStyle>
+        <GlobalStyle />
+        <App />
       </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>,
